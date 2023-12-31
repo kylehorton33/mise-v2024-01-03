584d9900236854bdb00081a94253513c049dd3db
Kyle Horton <kylehorton33@gmail.com>
2023-12-31 15:53:44 -0500

    feat: add app name, more color

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,14 +1,18 @@
-<script lang='ts'>
+<script lang="ts">
 	import { AppBar } from '@skeletonlabs/skeleton';
 	export let user: App.Locals['user'];
 </script>
 
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">(icon)</svelte:fragment>
-	(title)
+	<svelte:fragment slot="lead">(about)</svelte:fragment>
+	
+	<h1 class="h2 text-primary-500">mise</h1>
+
 	<svelte:fragment slot="trail">
 		{#if user}
-			<a href="/account" class="btn variant-outline-primary rounded-full w-4">{user.username[0].toUpperCase()}</a>
+			<a href="/account" class="btn variant-outline-primary rounded-full w-4"
+				>{user.username[0].toUpperCase()}</a
+			>
 		{:else}
 			<a href="/login" class="btn variant-outline-primary">Login</a>
 		{/if}
