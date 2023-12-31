1a316b1918c46559eb59b809027c637dd0757bf0
Kyle Horton <kylehorton33@gmail.com>
2023-12-31 15:53:16 -0500

    feat: truncate long named ingredients

--- frontend/src/routes/ingredients/IngredientCard.svelte
+++ frontend/src/routes/ingredients/IngredientCard.svelte
@@ -3,11 +3,11 @@
 	const { id, name, slug, unit } = ingredient;
 </script>
 
-<div class="card p-2">
+<div class="card p-2 max-w-72">
 	<span class="flex justify-between">
-		<h1 class="h3">{name}</h1>
+		<h1 class="h5 truncate">{name}</h1>
 		{#if unit}
-			<span class="badge variant-filled-secondary">{unit}</span>
+			<span class="badge variant-ghost-surface">{unit}</span>
 		{/if}
 	</span>
 </div>
