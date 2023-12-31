35c03bf87f076a3faa6d861986fc7fcbd6d0ec26
Kyle Horton <kylehorton33@gmail.com>
2023-12-31 12:25:13 -0500

    load ingredients

--- pocketbase/load.js
+++ pocketbase/load.js
@@ -1,9 +1,6 @@
 import PocketBase from 'pocketbase';
 import { ingredients } from './data.js';
 import { Ingredient } from './class.js';
-import myJSON from './exports/ingredients.json' assert { type: 'json' }
-
-console.log(myJSON)
 
 const { POCKETBASE_URL, PB_USERNAME, PB_PASSWORD } = process.env;
 const green = '\x1b[32m';
@@ -17,17 +14,17 @@ await pb.collection('users').authWithPassword(PB_USERNAME, PB_PASSWORD);
 // relevant fields and define keys
 // Scenario 1: Load from user input data (minimal fields, generate slug, 
 // let db assign id)
-//ingredients.forEach(async (i) => {
-//    const name = 'ingredients'
-//    const datum = (i.unit) ? new Ingredient(i, i.unit) : new Ingredient(i)
-//    try {
-//        const result = await pb.collection(name)
-//            .create(datum, { requestKey: datum.key() })
-//        console.log(green, `[CREATE] ${datum.key()} ${result.id}`)
-//    } catch (err) {
-//        const errMsg = JSON.stringify(err.response.data);
-//        console.log(red, `[ERROR] ${datum.key()} - ${errMsg}`)
-//    }
-//})
+ingredients.forEach(async (i) => {
+    const name = 'ingredients'
+    const datum = (i.unit) ? new Ingredient(i, i.unit) : new Ingredient(i)
+    try {
+        const result = await pb.collection(name)
+            .create(datum, { requestKey: datum.key() })
+        console.log(green, `[CREATE] ${datum.key()} ${result.id}`)
+    } catch (err) {
+        const errMsg = JSON.stringify(err.response.data);
+        console.log(red, `[ERROR] ${datum.key()} - ${errMsg}`)
+    }
+})
 // Scenario 2: Load from previous export (all fields, including id)
 //
