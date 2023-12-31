fd6e3e97dff6a759f9af43d857dc2bb070185d07
Kyle Horton <kylehorton33@gmail.com>
2023-12-31 16:00:33 -0500

    feat: static checkbox on IngredientCard

--- frontend/src/routes/ingredients/IngredientCard.svelte
+++ frontend/src/routes/ingredients/IngredientCard.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	// import { stockList } from '$lib/store'
+	// link up checkbox with stock status
 	export let ingredient;
 	const { id, name, slug, unit } = ingredient;
 </script>
 
 <div class="card p-2 max-w-72">
-	<span class="flex justify-between">
-		<h1 class="h5 truncate">{name}</h1>
+	<span class="flex items-center">
+		<input type="checkbox" name="" id="" class="checkbox mr-2">
+		<h1 class="h5 truncate grow">{name}</h1>
 		{#if unit}
 			<span class="badge variant-ghost-surface">{unit}</span>
 		{/if}
