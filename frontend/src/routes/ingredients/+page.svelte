80f05379838c2be88ffdf2140ef3d004917d5cd9
Kyle Horton <kylehorton33@gmail.com>
2023-12-31 13:13:12 -0500

    feat: IngredientCard

--- frontend/src/routes/ingredients/+page.svelte
+++ frontend/src/routes/ingredients/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { ingredients } from '$lib/store';
+    import IngredientCard from './IngredientCard.svelte';
 </script>
 
 <div class="container h-full w-full mx-auto flex justify-center p-4">
 	<div class="space-y-5">
 		<h1 class="h1 text-center">Ingredients</h1>
 		<p>Choose from this list of ingredients:</p>
-		<ul>
-			{#each $ingredients as { name }}
-				<li>{name}</li>
+		<ul class="space-y-2">
+			{#each $ingredients as ingredient}
+				<IngredientCard ingredient={ingredient} />
 			{/each}
 		</ul>
 	</div>
---
+++ frontend/src/routes/ingredients/IngredientCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let ingredient;
+	const { id, name, slug, unit } = ingredient;
+</script>
+
+<div class="card p-2">
+	<span class="flex justify-between">
+		<h1 class="h3">{name}</h1>
+		{#if unit}
+			<span class="badge variant-filled-secondary">{unit}</span>
+		{/if}
+	</span>
+</div>
