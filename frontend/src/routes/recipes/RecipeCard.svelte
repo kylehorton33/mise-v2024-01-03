07ab8839cf6c0cc83dfd2b47fa8278386ee0d23c
Kyle Horton <kylehorton33@gmail.com>
2024-01-01 18:13:49 -0500

    feat: style RecipeCard

--- frontend/src/routes/recipes/RecipeCard.svelte
+++ frontend/src/routes/recipes/RecipeCard.svelte
@@ -2,12 +2,15 @@
 	// TODO: import { stockList } from '$lib/store'
 	export let recipe: Recipe;
 	const { name, slug, ingredients } = recipe;
+	const flatIngredients = ingredients?.map((i) => i.ingredient.name).join(' | ');
 </script>
 
-<div class="card p-2 max-w-72 space-y-2">
-	<a href="/recipes/{slug}" class="flex items-center">
-		<h1 class="h5 truncate grow">{name}</h1>
-        <span class="badge variant-soft-warning">#</span>
-    </a>
-    <p class="text-xs truncate">{ingredients?.map((i) => i.ingredient.name).join(' | ')}</p>
+<div class="card p-4 max-w-72">
+	<a href="/recipes/{slug}" class="space-y-2">
+		<span class="flex items-center">
+			<h1 class="h5 truncate grow">{name}</h1>
+			<span class="badge variant-soft-warning">#</span>
+		</span>
+		<p class="text-xs truncate grow">{flatIngredients}</p>
+	</a>
 </div>
