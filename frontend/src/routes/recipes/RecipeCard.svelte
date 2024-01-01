3bb17bd0e8c15e5dc484f0c9802126c0269efe78
Kyle Horton <kylehorton33@gmail.com>
2024-01-01 18:32:51 -0500

    feat: calculate missing ingredients

--- frontend/src/routes/recipes/RecipeCard.svelte
+++ frontend/src/routes/recipes/RecipeCard.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
-	// TODO: import { stockList } from '$lib/store'
+	// DONE: import { stockList } from '$lib/store'
+	import { stockList } from '$lib/store';
+
 	export let recipe: Recipe;
+
 	const { name, slug, ingredients } = recipe;
 	const flatIngredients = ingredients?.map((i) => i.ingredient.name).join(' | ');
+	const missing = ingredients?.reduce((n, l) => n + +!$stockList[l.ingredient.id], 0);
 </script>
 
 <div class="card p-4 max-w-72">
 	<a href="/recipes/{slug}" class="space-y-2">
-		<span class="flex items-center">
+		<span class="flex items-start">
 			<h1 class="h5 truncate grow">{name}</h1>
-			<span class="badge variant-soft-warning">#</span>
+			{#if missing}
+				<span class="badge variant-outline-surface">{missing}</span>
+			{:else}
+			<span class="badge variant-filled-success">{missing}</span>
+			{/if}
 		</span>
 		<p class="text-xs truncate grow">{flatIngredients}</p>
 	</a>
