39ca9e571d3aced0143c598ac101ce85b367c8ab
Kyle Horton <kylehorton33@gmail.com>
2024-01-01 16:45:01 -0500

    feat: style a RecipeCard

--- frontend/src/routes/recipes/+page.svelte
+++ frontend/src/routes/recipes/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { recipes } from '$lib/store';
+	import RecipeCard from './RecipeCard.svelte';
+	
 </script>
 
 <div class="container h-full w-full mx-auto flex justify-center p-4">
 	<div class="space-y-5">
 		<h1 class="h1 text-center">Recipes</h1>
 		<p>Choose from this list of recipes:</p>
-		<ul>
-			{#each $recipes as { name, slug }}
-				<!-- TODO: style cards for recipes -->
-				<a href="/recipes/{slug}">
-					<li>{name}</li>
-				</a>
+		<ul class="space-y-2">
+			{#each $recipes as recipe}
+				<!-- DONE: style cards for recipes -->
+				<RecipeCard {recipe} />
+
 			{/each}
 		</ul>
         <p>Or create a new one:</p>
---
+++ frontend/src/routes/recipes/RecipeCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// TODO: import { stockList } from '$lib/store'
+	export let recipe: Recipe;
+	const { name, slug, ingredients } = recipe;
+</script>
+
+<div class="card p-2 max-w-72 space-y-2">
+	<a href="/recipes/{slug}" class="flex items-center">
+		<h1 class="h5 truncate grow">{name}</h1>
+        <span class="badge variant-soft-warning">#</span>
+    </a>
+    <p class="text-xs truncate">{ingredients?.map((i) => i.ingredient.name).join(' | ')}</p>
+</div>
