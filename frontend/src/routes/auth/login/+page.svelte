6e16d93ea692710d91c0132da6715f36cba960d3
Kyle Horton <kylehorton33@gmail.com>
2024-01-02 13:41:15 -0500

    feat: link register/login, format on card

--- frontend/src/routes/auth/login/+page.svelte
+++ frontend/src/routes/auth/login/+page.svelte
@@ -19,14 +19,19 @@
 	method="post"
 	class="container flex flex-col h-full items-center justify-center space-y-5"
 >
-	<h1 class="h1">Login</h1>
-	<div>
-		<label for="email" class="label">Email:</label>
-		<input type="email" name="email" id="email" class="input" />
+	<div class="card p-4 space-y-4">
+		<h1 class="h2 text-center">Login</h1>
+		<div>
+			<label for="email" class="label">Email:</label>
+			<input type="email" name="email" id="email" class="input" />
+		</div>
+		<div>
+			<label for="password" class="label">Password:</label>
+			<input type="password" name="password" id="password" class="input" />
+		</div>
+		<button class="btn variant-filled-success w-full">Login</button>
+		<div class="text-center">
+			<a href="/auth/register">Or register a new account</a>
+		</div>
 	</div>
-	<div>
-		<label for="password" class="label">Password:</label>
-		<input type="password" name="password" id="password" class="input" />
-	</div>
-	<button class="btn variant-filled-success">Login</button>
 </form>
--- frontend/src/routes/auth/register/+page.svelte
+++ frontend/src/routes/auth/register/+page.svelte
@@ -4,18 +4,23 @@
 	method="post"
 	class="container flex flex-col h-full items-center justify-center space-y-5"
 >
-	<h1 class="h1">Request Account</h1>
-	<div>
-		<label for="email" class="label">Email:</label>
-		<input type="email" name="email" id="email" class="input" />
+	<div class="card p-4 space-y-4">
+		<h1 class="h2 text-center">Register</h1>
+		<div>
+			<label for="email" class="label">Email:</label>
+			<input type="email" name="email" id="email" class="input" />
+		</div>
+		<div>
+			<label for="password" class="label">Password:</label>
+			<input type="password" name="password" id="password" class="input" />
+		</div>
+		<div>
+			<label for="passwordConfirm" class="label">Confirm Password:</label>
+			<input type="password" name="passwordConfirm" id="passwordConfirm" class="input" />
+		</div>
+		<button class="btn variant-filled-primary w-full">Register</button>
+		<div class="mt-4 text-center">
+			<a href="/auth/login">Or login to your account</a>
+		</div>
 	</div>
-	<div>
-		<label for="password" class="label">Password:</label>
-		<input type="password" name="password" id="password" class="input" />
-	</div>
-    <div>
-		<label for="passwordConfirm" class="label">Confirm Password:</label>
-		<input type="password" name="passwordConfirm" id="passwordConfirm" class="input" />
-	</div>
-	<button class="btn variant-filled-primary">Register</button>
 </form>
