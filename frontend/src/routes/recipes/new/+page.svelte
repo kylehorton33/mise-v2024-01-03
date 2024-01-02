ae7d4fe5d172fc3df76e1fdf0c083cf8fa8debf5
Kyle Horton <kylehorton33@gmail.com>
2024-01-01 21:21:53 -0500

    feat: format new Recipe form

--- frontend/src/routes/recipes/new/+page.svelte
+++ frontend/src/routes/recipes/new/+page.svelte
@@ -11,27 +11,40 @@
 	}
 </script>
 
-<form action="?/create" method="post" enctype="multipart/form-data" class="container flex flex-col mx-auto space-y-5 p-4">
+<form
+	action="?/create"
+	method="post"
+	enctype="multipart/form-data"
+	class="card flex flex-col space-y-5 m-4 p-4"
+>
 	<h1 class="h1">New Recipe</h1>
 	<div>
 		<label for="name" class="label">Name:</label>
-		<input type="text" name="name" id="name" class="input" />
+		<input type="text" name="name" id="name" class="input" placeholder="Recipe name..." />
 	</div>
 	<div>
 		<span class="label">Ingredients:</span>
 		<div class="space-y-2">
 			{#each lines as _, index}
-				<div class="grid grid-cols-5 gap-2">
-					<input type="number" step=0.25 class="input col-span-1" name="line[{index}][quantity]" />
-					<select class="select col-span-4" name="line[{index}][id]">
+				<div class="grid grid-cols-10 gap-2">
+					<input
+						type="number"
+						step="0.25"
+						class="input col-span-3"
+						name="line[{index}][quantity]"
+						placeholder="0.75"
+					/>
+					<select class="select col-span-7" name="line[{index}][id]">
+						<option value="" disabled selected>Ingredient (unit)</option>
 						{#each $ingredients as { id, name, unit }}
 							<option value={id}>{name} {unit ? `(${unit})` : ''}</option>
 						{/each}
 					</select>
 				</div>
 			{/each}
-			<button on:click|preventDefault={() => addNewLine()} class="btn variant-ghost-surface"
-				>Add Ingredient Line</button
+			<button
+				on:click|preventDefault={() => addNewLine()}
+				class="btn variant-ghost-primary hover:variant-filled-primary">Add Ingredient Line</button
 			>
 		</div>
 	</div>
@@ -49,5 +62,5 @@
 		<label for="image">Image:</label>
 		<input class="input" type="file" name="image" />
 	</div>
-	<button class="btn variant-ghost-success">Post Recipe</button>
+	<button class="btn variant-ghost-success hover:variant-filled-success">Post Recipe</button>
 </form>
