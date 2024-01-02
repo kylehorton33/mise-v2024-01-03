4d5e1a1a7deeddb141fce9d9c916c31b7a6300c0
Kyle Horton <kylehorton33@gmail.com>
2024-01-02 13:27:50 -0500

    feat: toast message if not verified

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -3,9 +3,10 @@
     console.log($page)
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto p-4 flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Error!</h1>
+        <p>{$page.error?.message}</p>
 		<a href="/" class="btn variant-filled-secondary w-full">
             Take me home
         </a>
--- frontend/src/routes/auth/login/+page.server.ts
+++ frontend/src/routes/auth/login/+page.server.ts
@@ -1,18 +1,20 @@
 import type { Actions } from "@sveltejs/kit"
-import { error, redirect } from "@sveltejs/kit"
+import { HttpError_1, error, redirect } from "@sveltejs/kit"
+import { ClientResponseError } from "pocketbase"
 
 export const actions = {
     login: async ({locals, request}) => {
         const body = Object.fromEntries(await request.formData())
         try {
             await locals.pb.collection('users').authWithPassword(body.email, body.password)
-            if (!locals.pb.authStore.model.verified) {
-                locals.pb.authStore.clear()
-                throw error(401, 'User not verified')
-            }
         } catch (e) {
             console.log(e)
-            throw error(401, `User ${body.email} could not be logged in`)
+            throw error(401, `User ${body.email} could not be logged in. Confirm username and password.`)
+        }
+
+        if (!locals.pb.authStore.model.verified) {
+            locals.pb.authStore.clear()
+            throw redirect(303, '/auth/login?msg=Your account must be verified first&color=variant-filled-error')
         }
 
         throw redirect(303, '/')
