e4f585fa745efd2451f648f3fb92a85947ed4c73
Kyle Horton <kylehorton33@gmail.com>
2024-01-02 14:07:42 -0500

    feat: add icon for login

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
+	import 'iconify-icon';
 	import { AppBar } from '@skeletonlabs/skeleton';
 	export let user: App.Locals['user'];
 </script>
 
-<AppBar gridColumns="grid-cols-3" slotLead="invisible" slotDefault="place-self-center" slotTrail="place-content-end">
+<AppBar
+	gridColumns="grid-cols-3"
+	slotLead="invisible"
+	slotDefault="place-self-center"
+	slotTrail="place-content-end"
+>
 	<svelte:fragment slot="lead">(left)</svelte:fragment>
-	
+
 	<h1 class="h2 text-primary-500">mise</h1>
 
 	<svelte:fragment slot="trail">
 		{#if user}
-			<a href="/auth/account" class="btn variant-outline-primary rounded-full w-4"
+			<a href="/auth/account" class="btn variant-filled-primary rounded-full w-4"
 				>{user.username[0].toUpperCase()}</a
 			>
 		{:else}
-			<a href="/auth/login" class="btn variant-outline-primary">Login</a>
+			<a href="/auth/login" class="btn variant-outline-primary rounded-full w-4">
+				<iconify-icon width="20" height="20" icon="material-symbols:lock-open-outline"></iconify-icon>
+			</a>
 		{/if}
 	</svelte:fragment>
 </AppBar>
