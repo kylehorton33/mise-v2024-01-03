ae711e31bb43342d8f42aca7c819fe2bca1af397
Kyle Horton <kylehorton33@gmail.com>
2024-01-02 20:16:22 -0500

    feat: style Account page

--- frontend/src/routes/auth/account/+page.svelte
+++ frontend/src/routes/auth/account/+page.svelte
@@ -1,14 +1,20 @@
-<script>
-    export let data;
-    const { user } = data;
+<script lang="ts">
+	export let data;
+	const { user } = data;
+
+	function dateFormat(date: string) {
+		return new Date(Date.parse(date)).toDateString();
+	}
 </script>
 
-<div class="flex flex-col w-full items-center justify-center space-y-5 p-4">
-    <h1 class="h1">Account:</h1>
-    <span class="code">
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-    </span>
-    <form action="?/logout" method="post">
-        <button class="btn variant-filled-error">Logout</button>
-    </form>
+<div class="flex flex-col h-full items-center justify-center space-y-5 p-4">
+	<div class="card flex flex-col space-y-5 p-4 font-mono">
+		<h1 class="h2 text-center">Account:</h1>
+		<p>Username: {user.username}</p>
+		<p>Email: {user.email}</p>
+		<p>Account Created: {dateFormat(user.created)}</p>
+		<form action="?/logout" method="post" class="h3 text-center">
+			<button class="btn variant-filled-error">Logout</button>
+		</form>
+	</div>
 </div>
