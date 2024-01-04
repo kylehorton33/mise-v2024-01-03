1d257ae71fe342bc5ec9c64ee549c8b1d36e67d3
Kyle Horton <kylehorton33@gmail.com>
2024-01-04 17:02:04 -0500

    feat: restyle not-verified page

--- frontend/src/routes/auth/not-verified/+page.svelte
+++ frontend/src/routes/auth/not-verified/+page.svelte
@@ -2,14 +2,12 @@
     import 'iconify-icon'
 </script>
 
-<div class="flex flex-col h-full justify-center items-center space-y-8">
-    <aside class="alert variant-filled-error w-72">
-        <!-- Icon -->
+<div class="flex flex-col h-full justify-center items-center space-y-16">
+    <aside class="alert variant-filled-surface w-72">
         <iconify-icon width="20" height="20" icon="solar:danger-triangle-linear"></iconify-icon>
-        <!-- Message -->
         <div class="alert-message">
             <h3 class="h3">Not Verified</h3>
-            <p>Sorry! This account has not been verified yet.</p>
+            <p>Sorry! Accounts are not automatically verified. Please contact the site owner to complete your account creation</p>
         </div>
     </aside>
     <a href="/" class="text-sm underline">Continue browsing as a guest</a>
