491dc6efd5bf7115b72540c09f3422be7900e45e
Kyle Horton <kylehorton33@gmail.com>
2024-01-04 16:35:52 -0500

    feat: not-verified page

--- frontend/src/routes/auth/not-verified/+page.svelte
+++ frontend/src/routes/auth/not-verified/+page.svelte
@@ -1,1 +1,16 @@
-Not Verified
+<script>
+    import 'iconify-icon'
+</script>
+
+<div class="flex flex-col h-full justify-center items-center space-y-8">
+    <aside class="alert variant-filled-error w-72">
+        <!-- Icon -->
+        <iconify-icon width="20" height="20" icon="solar:danger-triangle-linear"></iconify-icon>
+        <!-- Message -->
+        <div class="alert-message">
+            <h3 class="h3">Not Verified</h3>
+            <p>Sorry! This account has not been verified yet.</p>
+        </div>
+    </aside>
+    <a href="/" class="text-sm underline">Continue browsing as a guest</a>
+</div>
