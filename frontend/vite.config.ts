ac17ec6f3fa50f7457c24afa513e44e01773944a
Kyle Horton <kylehorton33@gmail.com>
2024-01-04 18:55:24 -0500

    dev: fix testing suite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.ts'],
+	}
 });
