73ac5c487f50b425d06123467d910721d5aa5dde
Kyle Horton <kylehorton33@gmail.com>
2024-01-04 19:34:41 -0500

    feat: tweak not-verified message

--- frontend/src/routes/auth/not-verified/+page.svelte
+++ frontend/src/routes/auth/not-verified/+page.svelte
@@ -1,14 +1,17 @@
 <script>
-    import 'iconify-icon'
+	import 'iconify-icon';
 </script>
 
 <div class="flex flex-col h-full justify-center items-center space-y-16">
-    <aside class="alert variant-filled-surface w-72">
-        <iconify-icon width="20" height="20" icon="solar:danger-triangle-linear"></iconify-icon>
-        <div class="alert-message">
-            <h3 class="h3">Not Verified</h3>
-            <p>Sorry! Accounts are not automatically verified. Please contact the site owner to complete your account creation</p>
-        </div>
-    </aside>
-    <a href="/" class="text-sm underline">Continue browsing as a guest</a>
+	<aside class="alert variant-filled-surface w-72">
+		<iconify-icon width="20" height="20" icon="solar:danger-triangle-linear"></iconify-icon>
+		<div class="alert-message">
+			<h3 class="h3">Not Verified</h3>
+			<p class="text-sm">
+				Sorry! Accounts must be approved before use. Please contact the site owner to complete your
+				account creation.
+			</p>
+		</div>
+	</aside>
+	<a href="/" class="text-sm underline">Continue browsing as a guest</a>
 </div>
