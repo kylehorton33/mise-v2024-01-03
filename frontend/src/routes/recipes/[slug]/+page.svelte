63189836b4ce45dd45d218dce854ec4026d388c6
Kyle Horton <kylehorton33@gmail.com>
2024-01-05 19:43:38 -0500

    feat: remove card style on mobile

--- frontend/src/routes/recipes/[slug]/+page.svelte
+++ frontend/src/routes/recipes/[slug]/+page.svelte
@@ -38,7 +38,7 @@
 
 {#if recipe}
 	<div
-		class="card mt-4 sm:mt-8 mx-4 sm:mx-auto p-4 sm:px-16 max-w-lg justify-center flex flex-col space-y-4"
+		class="sm:card sm:mt-8 mx-4 sm:mx-auto p-4 sm:px-16 max-w-lg justify-center flex flex-col space-y-4"
 	>
 		<h1 class="h2 text-center">{recipe.name}</h1>
 		<div class="text-center">
@@ -65,7 +65,7 @@
 		{#if recipe.image}
 			<img
 				src="{$PB_URL}/api/files/p7m0479jjpakez0/{recipe.id}/{recipe.image}?thumb=300x300"
-				class="w-72 self-center"
+				class="w-48 sm:72 self-center"
 				alt={recipe.name}
 			/>
 		{/if}
