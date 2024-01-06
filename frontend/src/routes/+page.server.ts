eadad8db74975036356627274ec6ef9c859d5dac
Kyle Horton <kylehorton33@gmail.com>
2024-01-06 12:03:23 -0500

    feat: change home page to 'ingredients'

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-    redirect(301, '/recipes')
+    redirect(301, '/ingredients')
 }) satisfies PageServerLoad;
