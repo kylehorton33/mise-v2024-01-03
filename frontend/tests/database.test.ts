191b4df279ef68b0049f7fe22a0327fdd329de16
Kyle Horton <kylehorton33@gmail.com>
2024-01-07 11:00:52 -0500

    test: create testing mode

--- frontend/tests/database.test.ts
+++ frontend/tests/database.test.ts
@@ -1,7 +1,8 @@
 // TODO: test content: connect to testing DB
 import { expect, test } from '@playwright/test';
 
-test('db test', async ({ page }) => {
-	await page.goto('/')
+test('page test', async ({ page }) => {
+    await page.goto('/')
     await expect(page.getByRole('heading', { name: 'Ingredients' })).toBeVisible();
-});
+    await expect(page.getByRole('listitem')).toHaveCount(1);
+})
