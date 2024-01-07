a5181d41ddb2b3850e98dd7d57c91df6e9d24a75
Kyle Horton <kylehorton33@gmail.com>
2024-01-07 13:06:13 -0500

    test: update plans to host testDB locally

--- frontend/tests/database.test.ts
+++ frontend/tests/database.test.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import Pocketbase from 'pocketbase'
 import { clearDB } from './db';
 
+// FIXME: Pockethost DB cannot handle rapid requests. Replace with a local PocketBase and make Pockethost
+// the default DB for staging/preview environment
 dotenv.config({ path: '.env.test' })
 const { VITE_POCKETBASE_URL, ADMIN_EMAIL, ADMIN_PASSWORD } = process.env
 
@@ -23,3 +25,17 @@ test('database is empty', async ({ page }) => {
     await page.goto('/recipes')
     await expect(page.getByRole('listitem')).toHaveCount(0);
 })
+
+test.describe('guest credentials', () => {
+    test('create ingredient not allowed', async ({ page }) => {
+        await page.goto('/ingredients');
+        await page.getByRole('button', { name: 'Add new ingredient' }).click();
+        await expect(page.getByText('You must be logged in to add')).toBeVisible();
+    });
+  
+    test('create recipe not allowed', async ({ page }) => {
+        await page.goto('/recipes');
+        await page.getByRole('button', { name: 'Add new recipe' }).click();
+        await expect(page.getByText('You must be logged in to add')).toBeVisible();
+    });
+  });
