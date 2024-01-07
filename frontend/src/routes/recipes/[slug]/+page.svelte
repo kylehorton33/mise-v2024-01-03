70718dda8ffd1a9efb1d376f6462a0752225a1c4
Kyle Horton <kylehorton33@gmail.com>
2024-01-06 21:58:06 -0500

    refactor: use `sendToast` function

--- frontend/src/routes/recipes/[slug]/+page.svelte
+++ frontend/src/routes/recipes/[slug]/+page.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
 	import { recipes, stockList, PB_URL } from '$lib/store';
 	import { page } from '$app/stores';
-	import { getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
-	import { fractionFormat } from '$lib/utils';
+	import { getToastStore } from '@skeletonlabs/skeleton';
+	import { fractionFormat, sendToast } from '$lib/utils';
 
-	const toastStore = getToastStore();
-
-	const toastName = $page.url.searchParams.get('new');
-	if (toastName) {
-		const t: ToastSettings = {
-			message: `Created: ${toastName}`,
-			background: 'variant-filled-success'
-		};
-		toastStore.trigger(t);
+	if ($page.url.searchParams.get('new')) {
+		const toastStore = getToastStore();
+		const message = 'New recipe created';
+		const background = 'variant-filled-success';
+		sendToast(toastStore, message, background)
 	}
 
 	const recipe = $recipes.find((recipe) => recipe.slug === $page.params.slug);
